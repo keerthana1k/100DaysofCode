0e9d9d92610bd31bfa9e56c0e09d2d4f10060b75
CodeWithKee <83121458+keerthana1k@users.noreply.github.com>
2022-02-14 22:34:02 +0530

    Add files via upload

---
+++ sum of two number.py
@@ -0,0 +1,9 @@
+#store the input
+num1 =input("Enter the First Number: ")
+num2 = input("Enter the Secound Number: ")
+
+#Add two numbers
+sum= float(num1) + float(num2)
+
+#display them
+print("Sum of {0} and {1} is {2}".format(num1, num2, sum))
